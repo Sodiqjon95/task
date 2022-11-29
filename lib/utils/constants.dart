0350221dfdf63770cb808ab60a404319d81fed81
Sodiqjon95 <sodiqdev95@gmil.com>
2022-11-29 11:14:07 +0500

    add

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,6 +5,8 @@ class RouteNames {
   static const String noInternet = '/no_internet';
   static const String offline = '/offline';
 }
+
+
 const String stripePublishableKey = "";
 const String defaultImage = "https://www.vecteezy.com/png/1200107-book";
 
